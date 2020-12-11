fdc660927e116c616fbcde0d4f7c0813a0ee34cf
Théophile Diot <theophile.diot900@gmail.com>
2020-12-11 18:14:22 +0100

    fix unhandled exeption

--- commands/miscellaneous/serverinfo.js
+++ commands/miscellaneous/serverinfo.js
@@ -13,46 +13,54 @@ module.exports = {
   },
 
   run: async (message) => {
-    let nbrRoles = 0;
-    let nbrVoiceChannels = 0;
-    let nbrTextChannels = 0;
+    try {
+      let nbrRoles = 0;
+      let nbrVoiceChannels = 0;
+      let nbrTextChannels = 0;
 
-    message.guild.channels.cache.forEach((channel) => {
-      if (channel.type.includes("voice")) {
-        nbrVoiceChannels += 1;
-      } else {
-        nbrTextChannels += 1;
-      }
-    });
+      message.guild.channels.cache.forEach((channel) => {
+        if (channel.type.includes("voice")) {
+          nbrVoiceChannels += 1;
+        } else {
+          nbrTextChannels += 1;
+        }
+      });
 
-    let sEmbed = new MessageEmbed()
-      .setColor(red_light)
-      .setTitle("Server Info")
-      .attachFiles(["./Photos/Logo_j4rvis.png"])
-      .attachFiles(["./Photos/Logo_INTECH_Activites_a_la_carte.png"])
-      .setThumbnail("attachment://Logo_INTECH_Activites_a_la_carte.png")
-      .setAuthor(
-        `${message.guild.name}`,
-        "attachment://Logo_INTECH_Activites_a_la_carte.png"
-      )
-      .addField("**Nom du serveur:**", `${message.guild.name}`, true)
-      .addField(
-        "**Création du serveur:**",
-        `${moment.utc(message.guild.createdAt).format("LL")}`,
-        true
-      )
-      .addField("**Propriétaire du serveur:**", `${message.guild.owner}`, true)
-      .addField(
-        "**Nombre d'utilisateurs:**",
-        `${message.guild.memberCount}`,
-        true
-      )
-      .addField("**Nombre de rôles:**", `${nbrRoles}`, true)
-      .addField("**Nombre de salon vocaux:**", `${nbrVoiceChannels}`, true)
-      .addField("**Nombre de salon textuels:**", `${nbrTextChannels}`, true)
-      .setFooter("J4RVIS", "attachment://Logo_j4rvis.png");
+      let sEmbed = new MessageEmbed()
+        .setColor(red_light)
+        .setTitle("Server Info")
+        .attachFiles(["./Photos/Logo_j4rvis.png"])
+        .attachFiles(["./Photos/Logo_INTECH_Activites_a_la_carte.png"])
+        .setThumbnail("attachment://Logo_INTECH_Activites_a_la_carte.png")
+        .setAuthor(
+          `${message.guild.name}`,
+          "attachment://Logo_INTECH_Activites_a_la_carte.png"
+        )
+        .addField("**Nom du serveur:**", `${message.guild.name}`, true)
+        .addField(
+          "**Création du serveur:**",
+          `${moment.utc(message.guild.createdAt).format("LL")}`,
+          true
+        )
+        .addField(
+          "**Propriétaire du serveur:**",
+          `${message.guild.owner}`,
+          true
+        )
+        .addField(
+          "**Nombre d'utilisateurs:**",
+          `${message.guild.memberCount}`,
+          true
+        )
+        .addField("**Nombre de rôles:**", `${nbrRoles}`, true)
+        .addField("**Nombre de salon vocaux:**", `${nbrVoiceChannels}`, true)
+        .addField("**Nombre de salon textuels:**", `${nbrTextChannels}`, true)
+        .setFooter("J4RVIS", "attachment://Logo_j4rvis.png");
 
-    await message.channel.send({ embed: sEmbed });
-    message.delete();
+      await message.channel.send({ embed: sEmbed });
+      message.delete();
+    } catch (e) {
+      console.log(e);
+    }
   },
 };
