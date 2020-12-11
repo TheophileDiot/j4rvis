4684628083f16a6c824dca08b89aecd63dc05315
Théophile Diot <theophile.diot900@gmail.com>
2020-12-11 18:09:32 +0100

    retrait de la liste des rôles dans serverinfo

--- commands/miscellaneous/serverinfo.js
+++ commands/miscellaneous/serverinfo.js
@@ -9,26 +9,17 @@ module.exports = {
     usage: "",
     category: "miscellaneous",
     accessableby: "Membres",
-    aliases: ["si", "serverdesc"]
+    aliases: ["si", "serverdesc"],
   },
-  
-  run: async (bot, message) => {
-    
-    let listRoles = "";
+
+  run: async (message) => {
     let nbrRoles = 0;
     let nbrVoiceChannels = 0;
     let nbrTextChannels = 0;
-    
-    message.guild.roles.cache.forEach((role) => {
-      if(!role.name.includes("@everyone") && !role.name.includes("Rythm") && !role.name.includes("Rythm 2") && !role.name.includes("MEE6") && !role.name.includes("Octave")){
-        listRoles = listRoles.concat(role.name, ", ");
-        nbrRoles += 1;
-      }
-    });
-    
+
     message.guild.channels.cache.forEach((channel) => {
-      if(channel.type.includes("voice")){
-         nbrVoiceChannels += 1;
+      if (channel.type.includes("voice")) {
+        nbrVoiceChannels += 1;
       } else {
         nbrTextChannels += 1;
       }
@@ -37,21 +28,31 @@ module.exports = {
     let sEmbed = new MessageEmbed()
       .setColor(red_light)
       .setTitle("Server Info")
-      .attachFiles(['./Photos/Logo_j4rvis.png'])
-      .attachFiles(['./Photos/Logo_INTECH_Activites_a_la_carte.png'])
-      .setThumbnail('attachment://Logo_INTECH_Activites_a_la_carte.png')
-      .setAuthor(`${message.guild.name}`, 'attachment://Logo_INTECH_Activites_a_la_carte.png')
+      .attachFiles(["./Photos/Logo_j4rvis.png"])
+      .attachFiles(["./Photos/Logo_INTECH_Activites_a_la_carte.png"])
+      .setThumbnail("attachment://Logo_INTECH_Activites_a_la_carte.png")
+      .setAuthor(
+        `${message.guild.name}`,
+        "attachment://Logo_INTECH_Activites_a_la_carte.png"
+      )
       .addField("**Nom du serveur:**", `${message.guild.name}`, true)
-      .addField("**Création du serveur:**", `${moment.utc(message.guild.createdAt).format("LL")}`, true)
+      .addField(
+        "**Création du serveur:**",
+        `${moment.utc(message.guild.createdAt).format("LL")}`,
+        true
+      )
       .addField("**Propriétaire du serveur:**", `${message.guild.owner}`, true)
-      .addField("**Nombre d'utilisateurs:**",`${message.guild.memberCount}`,true)
+      .addField(
+        "**Nombre d'utilisateurs:**",
+        `${message.guild.memberCount}`,
+        true
+      )
       .addField("**Nombre de rôles:**", `${nbrRoles}`, true)
-      .addField("**Liste des rôles:**", `${listRoles}`, true)
       .addField("**Nombre de salon vocaux:**", `${nbrVoiceChannels}`, true)
       .addField("**Nombre de salon textuels:**", `${nbrTextChannels}`, true)
-      .setFooter("J4RVIS", 'attachment://Logo_j4rvis.png');
-    
-    await message.channel.send({embed: sEmbed});
+      .setFooter("J4RVIS", "attachment://Logo_j4rvis.png");
+
+    await message.channel.send({ embed: sEmbed });
     message.delete();
-  }
+  },
 };
